a6911994761191774b2a7bbcbf106f106b44968f
Jorge Serrano <jsp@spain.conseur.org>
2022-07-16 15:40:20 +0200

    alert dialog box

--- src/README.md
+++ src/README.md
@@ -1,7 +1,11 @@
 # Samples
-
+- [`Alert Dialog Box`](#alert_dialog_box)
 - [`Bottom Navigation Bar`](#bottom_navigation_bar)
 - [`Horizotal Toolbar`](#horizontal_toolbar)
+
+## <a name="alert_dialog_box"></a>[**alert_dialog_box**](alert_dialog_box/)
+![Flutter Sample - Alert Dialog Box](images/flutter_sample-Alert_Dialog_Box.png)
+* This sample shows a typical Alert Dialog Box
 
 ## <a name="bottom_navigation_bar"></a>[**bottom_navigation_bar**](bottom_navigation_bar/)
 ![Flutter Sample - Bottom Navigation Bar](images/flutter_sample-Bottom_Navigation_Bar.png)
---
+++ src/alert_dialog/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alert Dialog Box',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Alert Dialog Box'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ButtonStyle flatButtonStyle = TextButton.styleFrom(
+    primary: Colors.white,
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+  );
+
+  showAlertDialogBox(BuildContext context) {
+    // set up the button
+    Widget okButton = TextButton(
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        backgroundColor: Colors.blue,
+        primary: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        ),
+      ),
+      child: const Text("OK"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Alert Dialog Box Title"),
+      content: const Text("Alert Dialog Box Text"),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: const Color.fromARGB(100, 120, 100, 20),
+      ),
+      backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showAlertDialogBox(context);
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blueGrey,
+            onPrimary: Colors.white,
+            shadowColor: Colors.black87,
+            padding: const EdgeInsets.all(16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            ),
+          ),
+          child: const Text("Show alert Dialog box"),
+        ),
+      ),
+    );
+  }
+}
